7be94d9c6932b31e9dd871d717d54a1c1fceabd5
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-12 11:39:06 +0000

    feat: add function to POST a form to enable checking of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,29 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 // define constant for modal
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-// wire up the button
+// wire up the 'Check AIP Key' button
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+// wire up the 'Run Checks' button
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-// create an asynchornous function to get data and display it a modal
+// create an asynchornous function to POST code and have it checked
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    // pasted code from JSHint instructions - POST
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+                    "Authorization": API_KEY,
+                 },
+        // attach the body of the form as the thing to be chacked
+        body: form,
+        })
+
+}
+
+
+// create an asynchornous function to get API key data and display it a modal
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
