55dc87b5ab4b33ce7a9792b227b494141348d0d3
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-12 12:09:37 +0000

    feat: add displayErrors function to show results of the check in a modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,42 @@ async function postForm(e) {
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-                    "Authorization": API_KEY,
-                 },
+            "Authorization": API_KEY,
+        },
         // attach the body of the form as the thing to be chacked
         body: form,
-        })
+    })
 
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+// create a displayErrors funcion - called above
+function displayErrors(data) {
+
+    let results = "";
+    let heading = `JSHints Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No Errors Reported</div>`;      
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 
